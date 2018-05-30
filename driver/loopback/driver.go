fb97b3988471b74f16252783d4fad8ea3e23763c
jakoblorz <jakob@lorz.org>
2018-05-30 15:32:17 +0200

    updated loopback driver to implement new architecture

--- driver/loopback/driver.go
+++ driver/loopback/driver.go
@@ -1,33 +1,60 @@
 package loopback
 
-import "github.com/jakoblorz/singapoor/stream"
+import (
+	"github.com/jakoblorz/singapoor/driver"
+	"github.com/jakoblorz/singapoor/stream"
+)
 
 type Driver struct {
-	channel chan interface{}
+	driverType driver.Type
+	channel    chan stream.Message
 }
 
-func NewDriver() (*Driver, error) {
+func NewDriver(driverType driver.Type) (*Driver, error) {
 	return &Driver{
-		channel: make(chan interface{}),
+		driverType: driverType,
+		channel:    make(chan stream.Message),
 	}, nil
 }
 
-func (d Driver) Open() error {
+func NewSingleThreadDriver() (driver.SingleThreadScaffold, error) {
+	return NewDriver(driver.SingleThreadDriver)
+}
+
+func NewMultiThreadDriver() (driver.MultiThreadScaffold, error) {
+	return NewDriver(driver.MultiThreadDriver)
+}
+
+func (d Driver) GetDriverType() driver.Type {
+	return d.driverType
+}
+
+func (d Driver) NotifyStreamClose() error {
 	return nil
 }
 
-func (d Driver) Close() error {
+func (d Driver) NotifyStreamOpen() error {
 	return nil
 }
 
-func (d Driver) Publisher() (stream.Publisher, error) {
-	return Publisher{
-		channel: d.channel,
-	}, nil
+func (d Driver) NotifyMessageTest() (bool, error) {
+	return true, nil
 }
 
-func (d Driver) Subscriber() (stream.Subscriber, error) {
-	return Subscriber{
-		channel: d.channel,
-	}, nil
+func (d Driver) NotifyMessageRecieve() (stream.Message, error) {
+	return <-d.channel, nil
+}
+
+func (d Driver) NotifyMessagePublish(msg stream.Message) error {
+
+	d.channel <- msg
+	return nil
+}
+
+func (d Driver) GetMessageWriterChannel() (chan<- stream.Message, error) {
+	return d.channel, nil
+}
+
+func (d Driver) GetMessageReaderChannel() (<-chan stream.Message, error) {
+	return d.channel, nil
 }
--- stream/message.go
+++ stream/message.go
@@ -1,3 +1,5 @@
 package stream
 
+// Message is a type alias for anything, adding extensibility if
+// more requirements are required on a single message
 type Message interface{}
