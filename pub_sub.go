79778328e5c1b09e1464fdb33b60e7577b2d8399
jakoblorz <jakob@lorz.org>
2018-06-03 22:37:25 +0200

    merged pub sub implementations

---
+++ pub_sub.go
@@ -0,0 +1,218 @@
+package brokerutil
+
+import (
+	"errors"
+
+	"github.com/jakoblorz/brokerutil/driver"
+)
+
+// PubSub is the common "gateway" to reach to interact with the message broker such
+// as Publish / Subscribe. Independently from the implementation of the driver, it
+// guarantees that the exposed functions will work as expected.
+type PubSub struct {
+	driver              driver.PubSubDriverScaffold
+	scheduler           scheduler
+	supportsConcurrency bool
+
+	terminate chan int
+	backlog   chan interface{}
+}
+
+// NewPubSubFromDriver creates a new PubSub from the provided driver
+//
+// Depending on the implementation of the driver (single- or multithreaded)
+// a different PubSub implementation will be chosen.
+func NewPubSubFromDriver(d driver.PubSubDriverScaffold) (*PubSub, error) {
+
+	var supportsConcurrency = false
+
+	if d.GetDriverType() == driver.MultiThreadPubSubDriver {
+		_, ok := d.(driver.MultiThreadPubSubDriverScaffold)
+
+		if !ok {
+			return nil, errors.New("could not cast driver to concurrency supporting driver")
+		}
+
+		supportsConcurrency = true
+	}
+
+	if d.GetDriverType() == driver.SingleThreadPubSubDriver {
+		_, ok := d.(driver.SingleThreadPubSubDriverScaffold)
+
+		if !ok {
+			return nil, errors.New("could not cast driver to plain driver")
+		}
+
+		supportsConcurrency = false
+	}
+
+	return &PubSub{
+		driver:              d,
+		scheduler:           newScheduler(),
+		supportsConcurrency: supportsConcurrency,
+		backlog:             make(chan interface{}),
+		terminate:           make(chan int),
+	}, nil
+}
+
+// SubscribeAsync creates a new callback function which is invoked
+// on any incomming messages.
+//
+// It returns a error chan which will contain
+// all occuring / returned errors of the SubscriberFunc. A nil error
+// indicates the auto-unsubscribe after the call of UnsubscribeAll().
+// Use the SubscriberIdentifier to Unsubscribe later.
+func (a PubSub) SubscribeAsync(fn SubscriberFunc) (chan error, SubscriberIdentifier) {
+	return a.scheduler.SubscribeAsync(fn)
+}
+
+// SubscribeSync creates a new callback function like SubscribeAsync().
+//
+// It will block until recieving error or nil in the error chan, then returns it.
+func (a PubSub) SubscribeSync(fn SubscriberFunc) error {
+	return a.scheduler.SubscribeSync(fn)
+}
+
+// Unsubscribe removes a previously added callback function from the invokation
+// loop.
+//
+// Use the SubscriberIdentifier created when calling SubscribeAsync(). It will
+// send a nil error in the callback function's error chan.
+func (a PubSub) Unsubscribe(identifier SubscriberIdentifier) {
+	a.scheduler.Unsubscribe(identifier)
+}
+
+// UnsubscribeAll removes all added callback functions from the invokation
+// loop.
+//
+// It will send a nil error in the callback's function's error chans.
+func (a PubSub) UnsubscribeAll() {
+	a.scheduler.UnsubscribeAll()
+}
+
+// Publish sends a message to the message broker.
+func (a PubSub) Publish(msg interface{}) error {
+	a.backlog <- msg
+	return nil
+}
+
+// ListenAsync starts the relay goroutine which uses the provided driver
+// to communicate with the message broker.
+func (a PubSub) ListenAsync() chan error {
+
+	errCh := make(chan error, 1)
+
+	go func() {
+		errCh <- a.ListenSync()
+	}()
+
+	return errCh
+}
+
+// ListenSync starts relay loops which use the provided driver to
+// communicate with the message broker.
+func (a PubSub) ListenSync() error {
+
+	defer a.scheduler.UnsubscribeAll()
+
+	if err := a.driver.OpenStream(); err != nil {
+		return err
+	}
+
+	defer a.driver.CloseStream()
+
+	//
+	// relay code for driver which supports concurrency
+	// such as recieving and writing from different goroutines
+	//
+	if a.supportsConcurrency {
+
+		d, ok := a.driver.(driver.MultiThreadPubSubDriverScaffold)
+
+		if !ok {
+			return errors.New("driver does not support concurrency, could not cast to correct interface")
+		}
+
+		tx, err := d.GetMessageWriterChannel()
+		if err != nil {
+			return err
+		}
+
+		rx, err := d.GetMessageReaderChannel()
+		if err != nil {
+			return err
+		}
+
+		go func() {
+
+			for {
+				select {
+				case <-a.terminate:
+					return
+				case msg := <-a.backlog:
+					tx <- msg
+				}
+			}
+		}()
+
+		for {
+			select {
+			case <-a.terminate:
+				return nil
+			case msg := <-rx:
+				err := a.scheduler.NotifySubscribers(msg)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	//
+	// relay code for driver which does not support concurrency
+	// such as recieving and writing from different goroutines
+	//
+
+	d, ok := a.driver.(driver.SingleThreadPubSubDriverScaffold)
+
+	if !ok {
+		return errors.New("driver does support concurrency but not with the pub-sub implementation, could not cast to correct interface")
+	}
+
+	for {
+		select {
+		case <-a.terminate:
+			return nil
+		case msg := <-a.backlog:
+			err := d.PublishMessage(msg)
+			if err != nil {
+				return err
+			}
+
+		default:
+
+			avail, err := d.CheckForPendingMessage()
+			if err != nil {
+				return err
+			}
+
+			if avail {
+				msg, err := d.ReceivePendingMessage()
+				if err != nil {
+					return err
+				}
+
+				a.scheduler.NotifySubscribers(msg)
+			}
+		}
+	}
+}
+
+// Terminate send a termination signal so that the blocking Listen will
+// be released.
+//
+// Subscribers will be unsubscribed was well.
+func (a PubSub) Terminate() error {
+	a.terminate <- 1
+	return nil
+}
---
+++ pub_sub_test.go
@@ -0,0 +1,310 @@
+package brokerutil
+
+import (
+	"testing"
+
+	"github.com/jakoblorz/brokerutil/driver"
+)
+
+type observableTestDriver struct {
+	driverType                          driver.PubSubDriverType
+	getDriverTypeCallbackFunc           func() driver.PubSubDriverType
+	getMessageWriterChannelCallbackFunc func() (chan<- interface{}, error)
+	getMessageReaderChannelCallbackFunc func() (<-chan interface{}, error)
+	closeStreamCallbackFunc             func() error
+	openStreamCallbackFunc              func() error
+	checkForPendingMessageCallbackFunc  func() (bool, error)
+	receivePendingMessageCallbackFunc   func() (interface{}, error)
+	publishMessageCallbackFunc          func(interface{}) error
+}
+
+func (d observableTestDriver) GetDriverType() driver.PubSubDriverType {
+
+	if d.getDriverTypeCallbackFunc != nil {
+		return d.getDriverTypeCallbackFunc()
+	}
+
+	return d.driverType
+}
+
+func (d observableTestDriver) CloseStream() error {
+
+	if d.closeStreamCallbackFunc != nil {
+		return d.closeStreamCallbackFunc()
+	}
+
+	return nil
+}
+
+func (d observableTestDriver) OpenStream() error {
+
+	if d.openStreamCallbackFunc != nil {
+		return d.openStreamCallbackFunc()
+	}
+
+	return nil
+}
+
+func (d observableTestDriver) CheckForPendingMessage() (bool, error) {
+
+	if d.checkForPendingMessageCallbackFunc != nil {
+		return d.checkForPendingMessageCallbackFunc()
+	}
+
+	return true, nil
+}
+
+func (d observableTestDriver) ReceivePendingMessage() (interface{}, error) {
+
+	if d.receivePendingMessageCallbackFunc != nil {
+		return d.receivePendingMessageCallbackFunc()
+	}
+
+	return nil, nil
+}
+
+func (d observableTestDriver) PublishMessage(msg interface{}) error {
+
+	if d.publishMessageCallbackFunc != nil {
+		return d.publishMessageCallbackFunc(msg)
+	}
+
+	return nil
+}
+
+func (d observableTestDriver) GetMessageWriterChannel() (chan<- interface{}, error) {
+
+	if d.getMessageWriterChannelCallbackFunc != nil {
+		return d.getMessageWriterChannelCallbackFunc()
+	}
+
+	return nil, nil
+}
+
+func (d observableTestDriver) GetMessageReaderChannel() (<-chan interface{}, error) {
+
+	if d.getMessageReaderChannelCallbackFunc != nil {
+		return d.getMessageReaderChannelCallbackFunc()
+	}
+
+	return nil, nil
+}
+
+type observableTestScheduler struct {
+	notifySchedulerCallbackFunc func(interface{}) error
+	subscribeAsyncCallbackFunc  func(SubscriberFunc) (chan error, SubscriberIdentifier)
+	subscribeSyncCallbackFunc   func(SubscriberFunc) error
+	unsubscribeCallbackFunc     func(SubscriberIdentifier)
+	unsubscribeAllCallbackFunc  func()
+}
+
+func (o observableTestScheduler) NotifySubscribers(msg interface{}) error {
+
+	if o.notifySchedulerCallbackFunc != nil {
+		return o.notifySchedulerCallbackFunc(msg)
+	}
+
+	return nil
+}
+
+func (o observableTestScheduler) SubscribeAsync(fn SubscriberFunc) (chan error, SubscriberIdentifier) {
+
+	if o.subscribeAsyncCallbackFunc != nil {
+		return o.subscribeAsyncCallbackFunc(fn)
+	}
+
+	return nil, ""
+}
+
+func (o observableTestScheduler) SubscribeSync(fn SubscriberFunc) error {
+
+	if o.subscribeSyncCallbackFunc != nil {
+		return o.subscribeSyncCallbackFunc(fn)
+	}
+
+	return nil
+}
+
+func (o observableTestScheduler) Unsubscribe(id SubscriberIdentifier) {
+
+	if o.unsubscribeCallbackFunc != nil {
+		o.unsubscribeCallbackFunc(id)
+	}
+}
+
+func (o observableTestScheduler) UnsubscribeAll() {
+
+	if o.unsubscribeAllCallbackFunc != nil {
+		o.unsubscribeAllCallbackFunc()
+	}
+}
+
+func TestNewPubSubFromDriver(t *testing.T) {
+
+	t.Run("should set supportsConcurrency to true when supporting driver is present", func(t *testing.T) {
+
+		d := observableTestDriver{
+			driverType: driver.MultiThreadPubSubDriver,
+		}
+
+		ps, err := NewPubSubFromDriver(d)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if ps.supportsConcurrency == false {
+			t.Error("NewPubSubFromDriver() did not set correct supportsConcurrency flag")
+		}
+	})
+
+	t.Run("should set supportsConcurrency to false when supporting driver is not present", func(t *testing.T) {
+
+		d := observableTestDriver{
+			driverType: driver.SingleThreadPubSubDriver,
+		}
+
+		ps, err := NewPubSubFromDriver(d)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if ps.supportsConcurrency == true {
+			t.Error("NewPubSubFromDriver() did not set correct supportsConcurrency flag")
+		}
+	})
+}
+
+func Test_PubSub_Publish(t *testing.T) {
+
+	t.Run("should enqueue message when publishing", func(t *testing.T) {
+
+		aw := &PubSub{
+			backlog: make(chan interface{}, 1),
+		}
+
+		var msgSend interface{} = "test message"
+
+		aw.Publish(msgSend)
+
+		msgReceive := <-aw.backlog
+
+		if msgSend != msgReceive {
+			t.Error("architectureAwarePubSub.Publish() did not enqueue message")
+		}
+	})
+}
+
+func Test_PubSub_SubscribeAsync(t *testing.T) {
+
+	t.Run("should invoke SubscribeAsync from scheduler", func(t *testing.T) {
+
+		var onSubscribeAsyncInvoked = false
+		var onSubscribeAsync = func(fn SubscriberFunc) (chan error, SubscriberIdentifier) {
+			onSubscribeAsyncInvoked = true
+			return nil, ""
+		}
+
+		aw := &PubSub{
+			scheduler: observableTestScheduler{
+				subscribeAsyncCallbackFunc: onSubscribeAsync,
+			},
+		}
+
+		aw.SubscribeAsync(func(msg interface{}) error {
+			return nil
+		})
+
+		if onSubscribeAsyncInvoked == false {
+			t.Error("architectureAwarePubSub.SubscribeAsync() did not invoke SubscribeAsync from scheduler")
+		}
+	})
+}
+
+func Test_PubSub_SubscribeSync(t *testing.T) {
+
+	t.Run("should invoked SubscribeSync from scheduler", func(t *testing.T) {
+
+		var onSubscribeSyncInvoked = false
+		var onSubscribeSync = func(fn SubscriberFunc) error {
+			onSubscribeSyncInvoked = true
+			return nil
+		}
+
+		aw := &PubSub{
+			scheduler: observableTestScheduler{
+				subscribeSyncCallbackFunc: onSubscribeSync,
+			},
+		}
+
+		aw.SubscribeSync(func(msg interface{}) error {
+			return nil
+		})
+
+		if onSubscribeSyncInvoked == false {
+			t.Error("architectureAwarePubSub.SubscribeSync() did not invoke SubscribeSync from scheduler")
+		}
+
+	})
+}
+
+func Test_PubSub_Unsubscribe(t *testing.T) {
+
+	t.Run("should invoke Unsubscribe from scheduler", func(t *testing.T) {
+
+		var onUnsubscribeInvoked = false
+		var onUnsubscribe = func(id SubscriberIdentifier) {
+			onUnsubscribeInvoked = true
+		}
+
+		aw := &PubSub{
+			scheduler: observableTestScheduler{
+				unsubscribeCallbackFunc: onUnsubscribe,
+			},
+		}
+
+		aw.Unsubscribe(SubscriberIdentifier("test-identifier"))
+
+		if onUnsubscribeInvoked == false {
+			t.Error("architectureAwarePubSub.Unsubscribe() did not invoke Unsubscribe from scheduler")
+		}
+
+	})
+}
+
+func Test_PubSub_UnsubscribeAll(t *testing.T) {
+
+	t.Run("should invoke UnsubscribeAll from scheduler", func(t *testing.T) {
+
+		var onUnsubscribeAllInvoked = false
+		var onUnsubscribeAll = func() {
+			onUnsubscribeAllInvoked = true
+		}
+
+		aw := &PubSub{
+			scheduler: observableTestScheduler{
+				unsubscribeAllCallbackFunc: onUnsubscribeAll,
+			},
+		}
+
+		aw.UnsubscribeAll()
+
+		if onUnsubscribeAllInvoked == false {
+			t.Error("architectureAwarePubSub.UnsubscribeAll() did not invoke UnsubscribeAll from scheduler")
+		}
+
+	})
+}
+
+func Test_PubSub_Terminate(t *testing.T) {
+
+	t.Run("should send termination signal in terminate channel", func(t *testing.T) {
+
+		aw := &PubSub{
+			terminate: make(chan int, 1),
+		}
+
+		aw.Terminate()
+
+		<-aw.terminate
+	})
+}
