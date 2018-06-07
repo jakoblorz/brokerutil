c23a3ba1a3b4a281f3a259d5bdfa0105ea79a045
jakoblorz <jakob@lorz.org>
2018-06-07 17:57:09 +0200

    added comments

--- driver_aware_pub_sub.go
+++ driver_aware_pub_sub.go
@@ -1,11 +1,15 @@
 package brokerutil
 
+// SubscriberFuncWithSource is the type of a subscriber func with provided driver information
 type SubscriberFuncWithSource func(interface{}, PubSubDriverScaffold) error
 
+// DriverAwarePubSub is an extension of PubSub with multiple drivers which enables its consumers
+// to control / be informed from which / to which broker a message is sent / received from
 type DriverAwarePubSub struct {
 	pubSub *PubSub
 }
 
+// NewDriverAwarePubSub creates a new DriverAwarePubSub from the provided drivers
 func NewDriverAwarePubSub(drivers ...PubSubDriverScaffold) (*DriverAwarePubSub, error) {
 
 	driverOptions := syntheticDriverOptions{
@@ -28,6 +32,12 @@ func NewDriverAwarePubSub(drivers ...PubSubDriverScaffold) (*DriverAwarePubSub, 
 	}, nil
 }
 
+// SubscribeAsync creates a new callback function which is invoked
+// on any incomming messages.
+//
+// It returns a error chan which will contain
+// all occuring / returned errors of the SubscriberFunc. A nil error
+// indicates unsubscription. Use the SubscriberIdentifier to unsubscribe later.
 func (a DriverAwarePubSub) SubscribeAsync(fn SubscriberFunc) (chan error, SubscriberIdentifier) {
 	return a.pubSub.SubscribeAsync(func(msg interface{}) error {
 
@@ -40,6 +50,12 @@ func (a DriverAwarePubSub) SubscribeAsync(fn SubscriberFunc) (chan error, Subscr
 	})
 }
 
+// SubscribeAsyncWithSource creates a new callback function which is invoked
+// on any incomming message with the driver ptr it came from (aka source).
+//
+// It returns a error chan which will contain
+// all occuring / returned errors of the SubscriberFunc. A nil error
+// indicates unsubscription. Use the SubscriberIdentifier to unsubscribe later.
 func (a DriverAwarePubSub) SubscribeAsyncWithSource(fn SubscriberFuncWithSource) (chan error, SubscriberIdentifier) {
 
 	return a.pubSub.SubscribeAsync(func(msg interface{}) error {
@@ -53,6 +69,9 @@ func (a DriverAwarePubSub) SubscribeAsyncWithSource(fn SubscriberFuncWithSource)
 	})
 }
 
+// SubscribeSync creates a new callback function like SubscriberAsync().
+//
+// It will block until receiving error or nil in error chan, then returns it.
 func (a DriverAwarePubSub) SubscribeSync(fn SubscriberFunc) error {
 
 	return a.pubSub.SubscribeSync(func(msg interface{}) error {
@@ -66,6 +85,10 @@ func (a DriverAwarePubSub) SubscribeSync(fn SubscriberFunc) error {
 	})
 }
 
+// SubscribeSyncWithSource creates a new callback function like SubscribeAsyncWithSource():
+// it will be invoked also with the driver ptr the message was received from
+//
+// It will block until receiving error or nil in error chan, then returns it.
 func (a DriverAwarePubSub) SubscribeSyncWithSource(fn SubscriberFuncWithSource) error {
 
 	return a.pubSub.SubscribeSync(func(msg interface{}) error {
@@ -79,20 +102,32 @@ func (a DriverAwarePubSub) SubscribeSyncWithSource(fn SubscriberFuncWithSource) 
 	})
 }
 
+// Unsubscribe removes a previously added callback function from the invokation
+// loop.
+//
+// Use the SubscriberIdentifier created when calling SubscribeAsync(). It will
+// send a nil error in the callback function's error chan.
 func (a DriverAwarePubSub) Unsubscribe(identifier SubscriberIdentifier) {
 	a.pubSub.Unsubscribe(identifier)
 }
 
+// UnsubscribeAll removes all added callback functions from the invokation
+// loop.
+//
+// It will send a nil error in the callback's function's error chans.
 func (a DriverAwarePubSub) UnsubscribeAll() {
 	a.pubSub.UnsubscribeAll()
 }
 
+// Publish sends a message to the message broker.
 func (a DriverAwarePubSub) Publish(msg interface{}) error {
 	return a.pubSub.Publish(syntheticMessageWithTarget{
 		message: msg,
 	})
 }
 
+// PublishWithTarget sends a message to the message broker. Specify the driver ptr to
+// send the message to.
 func (a DriverAwarePubSub) PublishWithTarget(msg interface{}, target PubSubDriverScaffold) error {
 	return a.pubSub.Publish(syntheticMessageWithTarget{
 		message: msg,
@@ -100,10 +135,14 @@ func (a DriverAwarePubSub) PublishWithTarget(msg interface{}, target PubSubDrive
 	})
 }
 
+// ListenAsync starts the relay goroutine which uses the provided drivers
+// to communicate with the message broker.
 func (a DriverAwarePubSub) ListenAsync() chan error {
 	return a.pubSub.ListenAsync()
 }
 
+// ListenSync starts relay loops which use the provided drivers to
+// communicate with the message broker.
 func (a DriverAwarePubSub) ListenSync() error {
 	return a.pubSub.ListenSync()
 }
